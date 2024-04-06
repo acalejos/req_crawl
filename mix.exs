dc0e6e7d38e5232dcf928d000ae8b1532b8a3651
Andres Alejos <andres.c.alejos.mil@army.mil>
2024-04-05 22:01:36 -0400

    0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReqCrawl.MixProject do
   def project do
     [
       app: :req_crawl,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       name: "ReqCrawl",
       start_permanent: Mix.env() == :prod,
